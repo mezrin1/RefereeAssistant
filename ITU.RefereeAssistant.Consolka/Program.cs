e7b07f3f509c1bb1afe14dfe8832e92e7da0db25
user <usermail>
2019-02-14 20:11:15 +0400

    Подключение dll

--- ITU.RefereeAssistant.Consolka/Program.cs
+++ ITU.RefereeAssistant.Consolka/Program.cs
@@ -8,6 +8,8 @@ using ITU.RefereeAssistant.BL;
 using ITU.RefereeAssistant.Domain.Models;
 using ITU.RefereeAssistant.Domain;
 using ITU.RefereeAssistant.Domain.TourType;
+using System.Reflection;
+using System.IO;
 
 
 namespace ITU.RefereeAssistant.Consolka
@@ -15,13 +17,64 @@ namespace ITU.RefereeAssistant.Consolka
 
     class Program
     {
+        //assembly - должно выполняться минимальное количество раз
+        static List<ITournamentType> GetTypes()
+        {
+            var result = new List<ITournamentType>();
+
+            var currentDirectory = Environment.CurrentDirectory;
+            var dlls = Directory.GetFiles(currentDirectory, "*.dll");
+
+            foreach (var dll in dlls)
+            {
+
+                var assembly = Assembly.LoadFile(dll);
+
+                //var assembly = Assembly.GetAssembly(typeof(ITournamentType));
+                //var assembly = Assembly.LoadFrom(@"D:\RefereeAssistant\ITU.RefereeAssistant.Consolka\bin\Debug\SuperType.dll");
+                
+                /* альтернативный вариант
+                return dlls
+                .Select(dll => Assembly.LoadFrom(dll))
+                .SelectMany(assem => assem.GetTypes())
+                .Where(type => type.GetInterfaces().Any(i => i.Name == "ITournamentType"))
+                .Select(tt => Activator.CreateInstance(tt))
+                .OfType<ITournamentType>()
+                .ToList();*/
+
+                var types = assembly.GetTypes();
+
+                foreach (var type in types)
+                {
+                    // проверить что этот класс является системой турнира
+                    // для это нужно понять какие интерфейсы он реализует
+                    var interfaces = type.GetInterfaces();
+                    // если класс реализует ITournamentType
+                    if (interfaces.Any(inter => inter.Name == "ITournamentType"))
+                    {
+                        // то нужно создать экземпляр класса
+                        var tournamentType = Activator.CreateInstance(type) as ITournamentType;
+                        // и добавить его в результативный список
+                        if (tournamentType != null)
+                        {
+                            result.Add(tournamentType);
+                        }
+                    }
+                }
+            }
+            return result;
+        }
 
         static void Main(string[] args)
         {
-            var tourTypes = new List<ITournamentType>()
-            {
-                new OlympicTourType()
-            };
+            //var tourTypes = GetTypes();
+            /*  var tourTypes = new List<ITournamentType>()
+              {
+                  new OlympicTourType()
+              };*/
+
+            var tourTypes = new List<ITournamentType>();
+            tourTypes = GetTypes();
 
             //Создание объекта класса TournamentService
             var ts = new TournamentService();
---
+++ ITU.RefereeAssistant.Domain/TourType/CircleTourType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ITU.RefereeAssistant.Domain.Models;
+
+namespace ITU.RefereeAssistant.Domain.TourType
+{
+    public class CircleTourType : ITournamentType
+    {
+        public string Name => "Круговая система"; // вместо return
+
+        public Round GetNextRound(IEnumerable<Player> players, IEnumerable<Round> rounds) //IEnumerable( в метод передается самое простое)
+        {
+
+            return null;
+        }
+    }
+}
